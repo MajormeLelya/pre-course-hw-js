ff73dc84aacd70ecc8e9078c034c7d4ff259090b
matebook <matebook@tech.com>
2023-10-20 23:23:27 +0300

    lesson-02 is done

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,15 +1,7 @@
-// code
+// // code
+
 let number = 1;
-
-number = number--;
-console.log(number); //1
-
-number = ++number;
-number = ++number;
-console.log(number); //3
-
-console.log(number); //3
-
-number = --number;
-number = --number;
-console.log(number); //1
+console.log(number++); // 1
+console.log(++number); // 3
+console.log(number--); // 3
+console.log(--number); // 1
