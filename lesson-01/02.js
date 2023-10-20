af9c15be6a86ec4cb7864f9de26454097710650d
matebook <matebook@tech.com>
2023-10-20 22:19:49 +0300

    lesson-01 is done

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,12 @@
 // code
+let number1 = Math.floor(Math.random() * 100);
+
+let ranNumber;
+
+if (number1 % 2 === 0) {
+  ranNumber = number1 + " :Это четное число";
+} else {
+  ranNumber = number1 + " :Это нечетное число";
+}
+
+console.log(ranNumber);
