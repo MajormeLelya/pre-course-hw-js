8c29ea457ca767282ec953c8dbe3221140081b9a
majorlelya <olya-mazhartseva@mail.ru>
2023-10-31 11:16:38 +0300

    04 fix bag

---
+++ lesson-01/02.js
@@ -0,0 +1,11 @@
+let number1 = Math.floor(Math.random() * 100);
+
+let ranNumber;
+
+if (number1 % 2 === 0) {
+  ranNumber = number1 + " :Это четное число";
+} else {
+  ranNumber = number1 + " :Это нечетное число";
+}
+
+console.log(ranNumber);
--- lesson-01/04.js
+++ lesson-01/04.js
@@ -1,22 +1,3 @@
-let number1 = Math.floor(Math.random() * 100);
-
-let ranNumber;
-
-if (number1 % 2 === 0) {
-  ranNumber = number1 + " :Это четное число";
-} else {
-  ranNumber = number1 + " :Это нечетное число";
-}
-
-console.log(ranNumber);
-
-let number = 1;
-
-console.log(number++);
-console.log(++number);
-console.log(number--);
-console.log(--number);
-
 let x = 5;
 x += 3;
 x *= 2;
