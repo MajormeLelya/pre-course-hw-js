e3099274b0e279bdd5481b716eba36dbc8699908
majorlelya <olya-mazhartseva@mail.ru>
2023-12-01 14:13:41 +0300

    hw03-01 is done

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,14 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+  const filteredColors = colors
+    .filter((color) => {
+      if (color === "черный" || color === "красный" || color === "желтый")
+        return color;
+    })
+    .join("-");
+
+  return filteredColors;
+}
+
+console.log(createColorString());
