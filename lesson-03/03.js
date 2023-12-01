f89ba742d1b5a454cb169689ea68c8c3eff45280
majorlelya <olya-mazhartseva@mail.ru>
2023-12-01 15:50:51 +0300

    hw02-03 is done

---
+++ lesson-03/03.js
@@ -0,0 +1,16 @@
+const fruits = [
+  "яблоко",
+  "банан",
+  "апельсин",
+  "манго",
+  "киви",
+  "ананас",
+  "виноград",
+  "груша",
+  "слива",
+  "персик",
+];
+
+const slicedFruits = fruits.slice(1, -2);
+
+console.log(slicedFruits);
