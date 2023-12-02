df1f0cc10630293c470507d761ff96a1d4055344
majorlelya <olya-mazhartseva@mail.ru>
2023-12-02 22:07:18 +0300

    hw03-07 is done

---
+++ lesson-03/07.js
@@ -0,0 +1,17 @@
+const numbers = [
+  [1, 2, 3, 4, 5],
+  [11, 20, 33, 40, 55],
+  [111, 200, 333, 400, 555],
+];
+
+function removeOddNumbers(arr) {
+  return arr.filter((num) => num % 2 === 0);
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i] = removeOddNumbers(numbers[i]);
+}
+
+for (let row of numbers) {
+  console.log(row);
+}
